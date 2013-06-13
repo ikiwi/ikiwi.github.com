490a8cc5943de5e383456ccabbd6841ce702e6c3
Greg Cooper <me@gre.gs>
2013-06-12 22:44:25 -0700

    Started Hammer site

--- app/controllers/admin_controller.rb
+++
@@ -1,6 +0,0 @@
-class AdminController < ApplicationController
-  skip_before_filter :authorize
-  before_filter :authorize_admin
-  def index
-  end
-end
--- db/migrate/20130521222252_add_email_id_to_user.rb
+++
@@ -1,5 +0,0 @@
-class AddEmailIdToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :email, :string
-  end
-end
